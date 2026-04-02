44cfe21e345b3cbd76f91bf221dfde5b5d4f418d
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-04-03 01:05:37 +0530

    Add layout

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,7 +2,6 @@ import Navigation from "../components/Navigation";
 
 export default function Page (){
     return <h1>
-        <Navigation/>
         Cabins page
     </h1>;
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import Link from "next/link"
-import Navigation from "./components/Navigation";
+
 
 export default function Home() {
   return (
     <div>
-      <Navigation/>
       <h1>The Wild Oasis. Welcome to paradise.</h1>
 
       <Link href="/cabins">Explore luxury cabins </Link>
