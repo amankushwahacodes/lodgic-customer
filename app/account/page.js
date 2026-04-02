67d6f315ae1c96c9fdff0d41bda6d8a915dabf1a
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-04-02 23:21:12 +0530

    Add routing

---
+++ app/account/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h3>Your account</h3>;
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,8 @@
+import Navigation from "../components/Navigation";
+
+export default function Page (){
+    return <h1>
+        <Navigation/>
+        Cabins page
+    </h1>;
+}
