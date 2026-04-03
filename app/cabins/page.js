818290ab5656ca077b61f4786f755cff4847b742
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-04-03 18:49:30 +0530

    RSC comp and data fetching

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,23 @@
+import Counter from "../components/Counter";
 import Navigation from "../components/Navigation";
 
-export default function Page (){
-    return <h1>
-        Cabins page
-    </h1>;
+export default async function Page() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+
+  const data = await res.json();
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <Counter users={data}/>
+      
+    </div>
+  );
 }
