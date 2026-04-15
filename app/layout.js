d02a145bca21f9ba419d4243fc759522b4d27e1d
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-04-15 19:50:51 +0530

    Setup Home Page

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-blue-400 text-gray-50 min-h-screen flex flex-col`}
+        className={`${josefin.className} antialiased bg-black text-gray-50 min-h-screen flex flex-col export default function Page() {
+  return (
+    <main className="mt-24">
+      <img src="/bg.png" alt="Mountains and forests with two cabins" />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <a
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
+    </main>
+  );
+}
+`}
       >
         <Header />
 
