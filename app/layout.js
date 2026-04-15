a86ded8837a68f56f9da311a6d4aac0c96971c7c
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-04-15 18:27:00 +0530

    Image optimizations and font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,16 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 
+import {Josefin_Sans} from "next/font/google"
 import "@/app/_styles/globals.css"
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets : ['latin'],
+  display : "swap",
+})
+
+console.log(josefin);
 
 export const metadata = {
   title: "The Wild Oasis",
@@ -10,13 +19,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-blue-400 text-gray-50 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-blue-400 text-gray-50 min-h-screen flex flex-col`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl  mx-auto ">{children}</main>
+        </div>
       </body>
     </html>
   );
