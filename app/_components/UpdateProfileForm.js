68aa8aca6b0a59b710e6bd1be9e3b34b909447da
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-05-18 19:28:22 +0530

    update guest

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,28 +1,34 @@
-"use client"
+import { updateGuest } from "../_lib/actions";
 
-import { useState } from "react";
+function UpdateProfileForm({ guest, children }) {
 
 
-function UpdateProfileForm({ children }) {
-  const [count, setCount] = useState();
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
-          disabled
-          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+          readOnly
+          defaultValue={fullName}
+          name="fullName"
+          className="px-5 py-3 w-full shadow-sm rounded-sm 
+    bg-gray-600 text-gray-400 cursor-not-allowed"
         />
       </div>
 
       <div className="space-y-2">
         <label>Email address</label>
         <input
-          disabled
-          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+          readOnly
+          defaultValue={email}
+          name="email"
+          className="px-5 py-3 w-full shadow-sm rounded-sm 
+    bg-gray-600 text-gray-400 cursor-not-allowed"
         />
       </div>
 
@@ -42,6 +48,7 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
+          defaultValue={nationalID}
           name="nationalID"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
