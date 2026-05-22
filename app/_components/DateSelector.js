91aa510c4d032e5a1562eb689bdb27925eee5b34
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-05-22 12:36:19 +0530

    reservation reminder

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,43 +1,56 @@
-"use client"
+"use client";
+import {
+  differenceInDays,
+  isPast,
+  isSameDay,
+  isWithinInterval,
+  startOfDay,
+} from "date-fns";
 
-import { isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { useReservation } from "./ReservationContext";
 
 function isAlreadyBooked(range, datesArr) {
   return (
-    range.from &&
-    range.to &&
+    range?.from &&
+    range?.to &&
     datesArr.some((date) =>
-      isWithinInterval(date, { start: range.from, end: range.to })
+      isWithinInterval(date, { start: range.from, end: range.to }),
     )
   );
 }
 
-function DateSelector({settings , cabin , bookedDates}) {
-  // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
-  const range = { from: null, to: null };
-
-  // SETTINGS
-  const {minBookingLength,maxBookingLength} = settings;
- 
+function DateSelector({ settings, cabin, bookedDates }) {
+  const { range, setRange, resetRange } = useReservation();
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+  const { regularPrice, discount } = cabin;
+  const numNights =
+    displayRange?.from && displayRange?.to
+      ? differenceInDays(displayRange.to, displayRange.from)
+      : 0;
+  const cabinPrice = numNights * (regularPrice - discount);
+  const { minBookingLength, maxBookingLength } = settings;
 
   return (
     <div className="flex flex-col justify-between">
       <DayPicker
-        className="pt-12 place-self-center"
         mode="range"
+        className="pt-12 place-self-center"
+        selected={displayRange}
+        onSelect={(selectedRange) => {
+          setRange(selectedRange);
+        }}
+        numberOfMonths={2}
         min={minBookingLength + 1}
         max={maxBookingLength}
-        fromMonth={new Date()}
         fromDate={new Date()}
-        toYear={new Date().getFullYear() + 5}
+        fromMonth={new Date()}
         captionLayout="dropdown"
-        numberOfMonths={2}
+        disabled={(curDate) =>
+          isPast(curDate) ||
+          bookedDates.some((date) => isSameDay(date, curDate))
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
@@ -68,10 +81,10 @@ function DateSelector({settings , cabin , bookedDates}) {
           ) : null}
         </div>
 
-        {range.from || range.to ? (
+        {range?.from || range?.to ? (
           <button
             className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-            onClick={() => resetRange()}
+            onClick={resetRange}
           >
             Clear
           </button>
--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,20 @@
-
-import {Josefin_Sans} from "next/font/google"
-import "@/app/_styles/globals.css"
+import { Josefin_Sans } from "next/font/google";
+import "@/app/_styles/globals.css";
 import Header from "./_components/Header";
+import { ReservationProvider } from "./_components/ReservationContext";
 
 const josefin = Josefin_Sans({
-  subsets : ['latin'],
-  display : "swap",
-})
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title : {
-    template : '%s | The Wild Oasis',
-    default : 'Welcome | The Wild Oasis'
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
   },
-  description : 'Luxurious cabin hotel , located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forests'
+  description:
+    "Luxurious cabin hotel , located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
@@ -25,7 +26,9 @@ export default function RootLayout({ children }) {
         <Header />
 
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl  mx-auto w-full ">{children}</main>
+          <main className="max-w-7xl  mx-auto w-full ">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
